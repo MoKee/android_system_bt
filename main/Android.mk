b43e45ccf2055ba136ff8a385362afa3634b9e69
Dan Pasanen <dan.pasanen@gmail.com>
2017-02-14 13:35:05 -0600

    Revert "Bluetooth-Wipower: Enable WiPower feature."
    This reverts commit 10df4bbd4a0872be624c3559a00dfaedc137d717.
    
    Change-Id: If4e248056df949c59098a5e0ba8b56aee23ab597

--- main/Android.mk
+++ main/Android.mk
@@ -26,16 +26,6 @@ LOCAL_SRC_FILES+= \
 LOCAL_SRC_FILES+= \
 	../udrv/ulinux/uipc.c
 
-ifeq ($(BOARD_USES_WIPOWER),true)
-ifneq ($(TARGET_SUPPORTS_WEARABLES),true)
-LOCAL_SRC_FILES += \
-       ../../../vendor/qcom/opensource/bluetooth/wipower-host/core/src/wipower.c
-else
-LOCAL_SRC_FILES += \
-       ../../../device/qcom/msm8909w/opensource/bluetooth/wipower-host/core/src/wipower.c
-endif
-endif
-
 LOCAL_C_INCLUDES+= . \
 	$(LOCAL_PATH)/../ \
 	$(LOCAL_PATH)/../bta/include \
@@ -65,18 +55,6 @@ LOCAL_C_INCLUDES+= . \
 	external/tinyxml2 \
     external/zlib \
     $(call include-path-for, audio-utils)
-
-ifeq ($(BOARD_USES_WIPOWER),true)
-ifneq ($(TARGET_SUPPORTS_WEARABLES),true)
-LOCAL_C_INCLUDES+= \
-       vendor/qcom/opensource/bluetooth/hal/include \
-       vendor/qcom/opensource/bluetooth/wipower-host/core/include
-else
-LOCAL_C_INCLUDES+= \
-       device/qcom/msm8909w/opensource/bluetooth/hal/include \
-       device/qcom/msm8909w/opensource/bluetooth/wipower-host/core/include
-endif
-endif
 
 LOCAL_SHARED_LIBRARIES := \
     libcutils \
